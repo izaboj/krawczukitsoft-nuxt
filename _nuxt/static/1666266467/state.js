9225b9286903d56e2f946c7eae653100b458b861
Izabela Krawczuk <izaboj@gmail.com>
2022-10-20 13:48:14 +0200

    28944ba

---
+++ _nuxt/static/1666266467/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fkrawczukitsoft-nuxt\u002F_nuxt\u002Fstatic\u002F1666266467",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fkrawczukitsoft-nuxt\u002F",assetsPath:"\u002Fkrawczukitsoft-nuxt\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"9fcb4020"}}}}(null));
